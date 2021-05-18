b935e26b22f33459a5f74631c133f4d47a3f5408
dacicus75 <dacicus75@hotmail.com>
2021-05-18 19:23:14 -0400

    added modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,21 @@
      delay: 10,
      time: 1000
     });
-
+    
+    let modalBtn = document.getElementById("modal-btn")
+    let modal = document.querySelector(".modal")
+    let closeBtn = document.querySelector(".close-btn")
+    modalBtn.onclick = function(){
+      modal.style.display = "block"
+    }
+    closeBtn.onclick = function(){
+      modal.style.display = "none"
+    }
+    window.onclick = function(e){
+      if(e.target == modal){
+        modal.style.display = "none"
+      }
+    }
 
     /* Back Top Link active
     ========================================================*/
